f0f5d49b828418a17aa8bd8bb357a034d06a88f2
SOTW123 <siyamszaman612@gmail.com>
2024-09-01 20:03:07 -0500

    hello

--- 2014-15/Dec/Cow_Jog/main.cpp
+++ 2014-15/Dec/Cow_Jog/main.cpp
@@ -5,6 +5,15 @@ ifstream fin("cowjog.in");
 ofstream fout("cowjog.out");
 
 int main() {
-
+	
     return 0;
 }
+
+/*
+x x x x       x
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
+  x   x x x   x
+    x     x x x x
+
+	ll
+*/
